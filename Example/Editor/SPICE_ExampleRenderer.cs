ef465bbf5907035a74eab6153739c9e62c413bcc
KIBA <kjh030529@gmail.com>
2022-07-02 22:49:54 +0900

    Add Example Assets

---
+++ Example/Editor/SPICE_ExampleRenderer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using K13A.BehaviourEditor;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SPICE_ExampleScript))]
+public class SPICE_ExampleRenderer : Editor
+{
+    public SPICE_ExampleScript origin;
+
+    public static List<TitleOption> Options = new List<TitleOption>();
+
+    public override void OnInspectorGUI()
+    {
+        origin = (SPICE_ExampleScript)target;
+
+        GUI.skin.label.richText = true;
+        GUILayout.Space(20);
+        var titleStyle = new GUIStyle();
+        titleStyle.normal.background = null;
+        titleStyle.alignment = TextAnchor.MiddleCenter;
+        GUILayout.Box(origin.Logo, titleStyle, GUILayout.ExpandWidth(true), GUILayout.Height(100));
+        GUILayout.Space(20);
+
+
+
+        EditorUtil.MenuBox(origin.MenuBoxTitle, () =>
+        {
+            origin.MenuBoxTitle = EditorGUILayout.TextField("Menu Box Title", origin.MenuBoxTitle);
+            
+            GUILayout.Space(20);
+            
+            EditorUtil.SubMenuBox(origin.SubMenuBoxTitle, () =>
+            {
+                origin.SubMenuBoxTitle = EditorGUILayout.TextField("Sub Menu Box Title", origin.SubMenuBoxTitle);
+            });
+        });
+        
+        GUILayout.Space(20);
+
+        EditorUtil.MenuBox("Menu Option Buttons", () =>
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("+"))
+            {
+                Options.Add(new TitleOption(new GUIContent($"new Option{Options.Count}"), () =>
+                {
+                    var winbow = EditorWindow.GetWindow(typeof(DetachedEditor)) as DetachedEditor;
+                    winbow.Render = () =>
+                    {
+                        EditorGUILayout.LabelField($"new Function");
+                    };
+                    winbow.titleContent = new GUIContent($"new Option");
+                }));
+            }
+            if (GUILayout.Button("-"))
+            {
+                Options.RemoveAt(Options.Count - 1);
+            }
+            EditorGUILayout.EndHorizontal();
+        }, null, Options.ToArray());
+
+        GUILayout.Space(20);
+        
+        origin.isFold = EditorUtil.FoldoutMenuBox("Foldout Menu Box", origin.isFold,() =>
+        {
+            EditorUtil.SubMenuBox("Sub Menu Box", () =>
+            {
+                EditorGUILayout.LabelField($"is Foldout Box!");
+            });
+        });
+    }
+}
---
+++ Example/SPICE_ExampleScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace K13A.BehaviourEditor
+{
+    public class SPICE_ExampleScript : MonoBehaviour
+    {
+        public Texture2D Logo;
+        public string MenuBoxTitle = "Menu Box 1";
+        public string SubMenuBoxTitle = "Sub Menu Box 1-1";
+        public bool isFold = true;
+    }
+}
